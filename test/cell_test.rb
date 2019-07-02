ecb78ef67d6778e94069767a33fc509f6330e744
Tylor Schafer <tylorschafer@gmail.com>
2019-07-01 19:54:30 -0600

    Add more cell tests

--- test/cell_test.rb
+++ test/cell_test.rb
@@ -8,6 +8,7 @@ class CellTest < Minitest::Test
 
   def setup
     @cell = Cell.new('B4')
+    @cruiser = Ship.new("Cruiser", 3)
   end
 
   def test_cell_exsists
@@ -27,15 +28,25 @@ class CellTest < Minitest::Test
   end
 
   def test_cell_can_add_ship
-    cruiser = Ship.new("Cruiser", 3)
-    @cell.place_ship(cruiser)
+    @cell.place_ship(@cruiser)
     assert_instance_of Ship, @cell.ship
   end
 
   def test_cell_is_not_empty_with_ship_placed
-    cruiser = Ship.new("Cruiser", 3)
-    @cell.place_ship(cruiser)
+    @cell.place_ship(@cruiser)
     assert_equal false, @cell.empty?
   end
 
+  def test_cell_knows_when_fired_upon
+    @cell.place_ship(@cruiser)
+    assert_equal false, @cell.fired_upon?
+    @cell.fire_upon
+    assert_equal true, @cell.fired_upon?
+  end
+
+  def test_ship_on_cell_takes_damage_when_fired_upon
+    @cell.place_ship(@cruiser)
+    @cell.fire_upon
+    assert_equal 2, @ship.health
+  end
 end
