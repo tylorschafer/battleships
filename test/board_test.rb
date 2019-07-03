ea41be79d621f2dc7a18f0837e3efcffd6d0b213
Corina Allen <StarPerfect@gmail.com>
2019-07-03 16:06:19 -0600

    Pair programmed to revisit our conceptualized content and make small changes
    Co-authored-by: Tylor Schafer <3358870+tylorschafer@users.noreply.github.com>

--- test/board_test.rb
+++ test/board_test.rb
@@ -3,6 +3,7 @@ require 'minitest/pride'
 require './lib/board'
 require './lib/ship'
 require './lib/cell'
+require './lib/coordinates'
 require 'pry'
 
 class BoardTest < Minitest::Test
@@ -20,30 +21,38 @@ class BoardTest < Minitest::Test
     assert Hash.new, @board
   end
 
+  def test_cells_created
+    assert @board.create_cells.include?('A1')
+  end
+
   def test_board_has_16_cells_and_each_key_is_cell
-    assert_equal 16, @board.cells.count
+    assert_equal 16, @board.create_cells.count
   end
 
   def test_validate_coordinate?
-    assert_equal true, @board.validate_coordinate?(:A1)
+    assert @board.validate_coordinate?('A1')
+    assert @board.validate_coordinate?('D4')
+    refute @board.validate_coordinate?('A5')
+    refute @board.validate_coordinate?('E1')
+    refute @board.validate_coordinate?('A22')
   end
 
   def test_valid_placement_coordinates_same_as_ship_length
-    assert_equal false, @board.valid_placement?(@cruiser, [:A1, :A2])
-    assert_equal true, @board.valid_placement?(@submarine, [:A1, :A2])
+    assert_equal false, @board.valid_placement?(@cruiser, ['A1', 'A2'])
+    assert_equal true, @board.valid_placement?(@submarine, ['A1', 'A2'])
   end
 
   def test_valid_placement_coordinates_are_consecutive
-    assert_equal false, @board.valid_placement?(@cruiser, [:A1, :A2, :A4])
-    assert_equal true, @board.valid_placement?(@cruiser, [:A1, :A2, :A3])
-    assert_equal false, @board.valid_placement?(@submarine, [:A1, :C1])
-    assert_equal false, @board.valid_placement?(@cruiser, [:A3, :A2, :A1])
+    assert_equal false, @board.valid_placement?(@cruiser, ['A1', 'A2', 'A4'])
+    assert_equal true, @board.valid_placement?(@cruiser, ['A1', 'A2', 'A3'])
+    assert_equal false, @board.valid_placement?(@submarine, ['A1', 'C1'])
+    assert_equal false, @board.valid_placement?(@cruiser, ['A3', 'A2', 'A1'])
   end
 
   def test_valid_placement_coordinates_are_not_diagonal
     skip
-    assert_equal false, @board.valid_placement?(@cruiser, [:A3, :B2, :C1])
-    assert_equal false, @board.valid_placement?(@submarine, [:C2, :D3])
+    assert_equal false, @board.valid_placement?(@cruiser, ['A3', 'B2', 'C1'])
+    assert_equal false, @board.valid_placement?(@submarine, ['C2', 'D3'])
   end
 
 end
