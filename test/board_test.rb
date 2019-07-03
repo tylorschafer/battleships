4359036c6b76e338fa8d4020769ecd28e8deba6d
Tylor Schafer <tylorschafer@gmail.com>
2019-07-02 18:45:50 -0600

    All current board tests now passing

--- test/board_test.rb
+++ test/board_test.rb
@@ -17,11 +17,10 @@ class BoardTest < Minitest::Test
   end
 
   def test_board_has_16_cells_and_each_key_is_cell
-    # assert_equal 16, @board.count
-    assert_equal Cell.new, @board["A1"]
+    assert_equal 16, @board.cells.count
   end
 
   def test_validate_coordinate?
-    assert_equal true, @board.validate_coordinate?("A1")
+    assert_equal true, @board.validate_coordinate?(:A1)
   end
 end
