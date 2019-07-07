425ad3b142a53368b816a1987c573667d7337899
Corina Allen <StarPerfect@gmail.com>
2019-07-07 03:59:06 -0600

    User Class and Test up to Cruiser Placement almost working, just need to correct board rendering when cruiser placed in valid location

---
+++ lib/user.rb
@@ -0,0 +1,24 @@
+class User
+  attr_reader :board, :cruiser, :submarine
+
+  def initialize
+    @board = Board.new
+    @board.create_cells
+    @cruiser = Ship.new('Cruiser', 3)
+    @submarine = Ship.new('Submarine', 2)
+  end
+
+  def cruiser_placement
+    # Iteration over this until uder provides valid coordinates
+    puts 'Enter the squares for the Cruiser (3 spaces):'
+    selection = gets.chomp
+    selection_array = selection.split(" ")
+    if @board.valid_placement?(@cruiser, selection_array)
+      @board.place(@cruiser, selection_array)
+      @board.render
+      return true
+    else
+      puts "Those are invalid coordinates. Please try again:"
+    end
+  end
+end
