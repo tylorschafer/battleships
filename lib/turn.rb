46c96567ba6a7655e682dc04ed58a3dc2d110e01
Corina Allen <StarPerfect@gmail.com>
2019-07-09 12:01:52 -0600

    Work to display shot taken result
    Co-authored-by: Tylor Schafer <3358870+tylorschafer@users.noreply.github.com>

--- lib/turn.rb
+++ lib/turn.rb
@@ -12,34 +12,62 @@ class Turn
   end
 
   def display_both_boards
+    puts '=============COMPUTER BOARD============='
     @computer.board.render
+    puts '==============PLAYER BOARD=============='
     @user.board.render(true)
   end
 
   def user_fires_shot
-    puts 'Enter the coodinate for your shot:'
+    puts 'Enter the coordinate for your shot:'
     valid_selection = false
     until valid_selection == true
-      fire_selection = 'C1'
-      if @computer.board.valid_coordinate?(fire_selection) && @computer.board.cells[fire_selection].fired_upon? == false
-        @computer.board.cells[fire_selection.upcase].fire_upon
+      @fire_selection = gets.chomp.upcase
+      if @computer.board.valid_coordinate?(@fire_selection) && @computer.board.cells[@fire_selection].fired_upon? == false
+        @computer.board.cells[@fire_selection].fire_upon
         valid_selection = true
       else
-        puts 'Please enter valid coordinate:'
-        next
+        puts 'You have already fired upon this location. Please select another:'
+        @fire_selection = gets.chomp.upcase
       end
+      ### still need to loop the invalid selection and fire upon once valid
+
+    end
+    puts "Your shot on #{@fire_selection} was a #{user_shot_result}."
+  end
+
+  def user_shot_result
+    case
+    when @computer.board.cells[@fire_selection].cell_contents == []
+      puts 'miss'
+    when @computer.board.cells[@fire_selection].cell_contents == Ship
+      puts 'hit'
+    when @computer.board.cells[@fire_selection].cell_contents == Ship && (@computer.cruiser.sunk? || @computer.submarine.sunk?)
+      puts 'hit and sunk a ship'
+    end
+  end
+
+  def computer_shot_result
+    case
+    when @user.board.cells[@computer_selection].cell_contents == []
+      puts 'miss'
+    when @user.board.cells[@computer_selection].cell_contents == Ship
+      puts 'hit'
+    when @user.board.cells[@computer_selection].cell_contents == Ship && (@user.cruiser.sunk? || @user.submarine.sunk?)
+      puts 'hit and sunk a ship'
     end
   end
 
   def computer_fires_shot
-    computer_selection = @user.board.coordinates.sample
     valid_selection = false
     until valid_selection == true
-      if @user.board.cells[computer_selection].fired_upon? == false
-        @user.board.cells[computer_selection].fire_upon
+      @computer_selection = @user.board.coordinates.sample
+      if @user.board.cells[@computer_selection].fired_upon? == false
+        @user.board.cells[@computer_selection].fire_upon
         valid_selection = true
       end
     end
+    puts "My shot on #{@computer_selection} was a #{computer_shot_result}."
   end
 
   def take
