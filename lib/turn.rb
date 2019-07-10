e69aeea878fd14b55c51286bdde7d79a1d41277c
Tylor Schafer <tylorschafer@gmail.com>
2019-07-10 12:54:27 -0600

    Clean up computer_smart_fire

--- lib/turn.rb
+++ lib/turn.rb
@@ -13,9 +13,9 @@ class Turn
   def initialize(computer, user)
     @computer = computer
     @user = user
-    @user_coordinates = @user.board.coordinates
     @turn_collection = ['xxx']
     @coordinate_collection = []
+    @user_cells = @user.board.cells
   end
 
   def display_both_boards
@@ -72,36 +72,37 @@ class Turn
     end
   end
 
-    def computer_smart_firing
-      @last_coordinate = @user_coordinates.find{|coor|(coor == @coordinate_collection.last)}
-      valid_choice = false
-      until valid_choice == true
-        if @turn_collection.last.include?('hit') == true && @turn_collection.last.include?('sunk') == false
-            @computer_selection = @user_coordinates[@user_coordinates.index(@last_coordinate) + 1]
-          if @user.board.cells[@computer_selection].fired_upon? == false
-            @user.board.cells[@computer_selection].fire_upon
-            valid_choice = true
-            @coordinate_collection << @computer_selection
-          else
-            @computer_selection = @user.board.coordinates.sample
-            if @user.board.cells[@computer_selection].fired_upon? == false
-              @user.board.cells[@computer_selection].fire_upon
-              valid_choice = true
-              @coordinate_collection << @computer_selection
-            end
-          end
+def computer_take_shot
+  @user_cells[@computer_selection].fire_upon
+  @valid_choice = true
+  @coordinate_collection << @computer_selection
+end
+
+  def computer_smart_firing
+    user_coordinates = @user.board.coordinates
+    last_coordinate = user_coordinates.find{|coor|(coor == @coordinate_collection.last)}
+    @valid_choice = false
+    until @valid_choice == true
+      if @turn_collection.last.include?('hit') == true && @turn_collection.last.include?('sunk') == false
+          @computer_selection = user_coordinates[user_coordinates.index(last_coordinate) + 1]
+        if @user_cells[@computer_selection].fired_upon? == false
+          computer_take_shot
         else
           @computer_selection = @user.board.coordinates.sample
-          if @user.board.cells[@computer_selection].fired_upon? == false
-            @user.board.cells[@computer_selection].fire_upon
-            valid_choice = true
-            @coordinate_collection << @computer_selection
+          if @user_cells[@computer_selection].fired_upon? == false
+            computer_take_shot
           end
         end
+      else
+        @computer_selection = @user.board.coordinates.sample
+          if @user_cells[@computer_selection].fired_upon? == false
+          computer_take_shot
+          end
       end
-      computer_response = puts "My shot on #{@computer_selection} was a #{computer_shot_result}."
-      computer_response
     end
+    computer_response = puts "My shot on #{@computer_selection} was a #{computer_shot_result}."
+    computer_response
+  end
 
   def take
     user_fires_shot
